b7fc973f360eaa0931bfd0b6d580e22061868280
Imesh <imeshsps@gmail.com>
2025-09-27 11:26:19 +0530

    added github username

--- misc-modules/hellop.c
+++ misc-modules/hellop.c
@@ -18,7 +18,7 @@ MODULE_LICENSE("Dual BSD/GPL");
  * A couple of parameters that can be passed in: how many times we say
  * hello, and to whom.
  */
-static char *whom = "world";
+static char *whom = "imeshsps";
 static int howmany = 1;
 module_param(howmany, int, S_IRUGO);
 module_param(whom, charp, S_IRUGO);
